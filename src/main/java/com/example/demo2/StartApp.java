6c94c8586e6c7159d565d3b13d92288c9f9cecb1
Luddekudde33 <ludvig.ostholm@gmail.com>
2025-05-08 14:46:15 +0200

    namnbyte

--- src/main/java/com/example/demo2/StartApp.java
+++ src/main/java/com/example/demo2/StartApp.java
@@ -1,14 +1,11 @@
 package com.example.demo2;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class LoginApp extends Application {
+public class StartApp extends Application {
 
 
     @Override
